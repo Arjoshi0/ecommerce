f50a34964ae4d4800592ffded91781cf1d8df69c
Arjoshi0 <aarushrj.12@gmail.com>
2024-08-12 18:42:01 -0500

    first commit

---
+++ essence/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path('blog', views.blog, name='blog'),
+    path('blog_details', views.blog_details, name='blog_details'),
+    path('checkout', views.checkout, name='checkout'),
+    path('contact', views.contact, name='contact'),
+    path('home', views.home, name='home'),
+    path('product_details', views.product_details, name='product_details'),
+    path('regular_page', views.regular_page, name='regular_page'),
+    path('shop', views.shop, name='shop'),
+]
---
+++ essence/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, "home/index.html")
+
+def contact(request):
+    return render(request, "contact/index.html")
+
+def blog(request):
+    return render(request, "blog/index.html")
+
+def blog_details(request):
+    return render(request, "blog_details/index.html")
+
+def checkout(request):
+
+    return render(request, "checkout/index.html")
+
+def home(request):
+    return render(request, "home/index.html")
+
+def product_details(request):
+    return render(request, "product_details/index.html")
+
+def regular_page(request):
+    return render(request, "reqular_page/index.html")
+
+def shop(request):
+    return render(request, "shop/index.html")
