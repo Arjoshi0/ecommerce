77287f445d035762e0737547929b7636a36de2ea
Rocky Pukar <ckrp2018@gmail.com>
2024-08-18 05:36:14 +0545

    WIP: create models

--- essence/models.py
+++ essence/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-  name = models.CharField(max_length=20)
+  name = models.CharField(max_length=50)
   description = models.CharField(max_length=200)
   price = models.FloatField()
   stock = models.IntegerField()
+
+class Customer(models.Model):
+  name = models.CharField(max_length=50)
+  email = models.EmailField(unique=True)
+  phone_no = models.IntegerField(unique=True)
+  address = models.CharField(max_length=200)
